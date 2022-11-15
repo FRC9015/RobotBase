24d4a2e9406bce90185b8e0e01beea66b1ce66de
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-14 22:12:09 -0500

    replace abstract class extends with interface implements

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -1,54 +1,43 @@
-// Copyright (c) FIRST and other WPILib contributors.
-
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 
+import java.util.Set;
 
+public class ExampleCommand implements Command {
+    private final ExampleSubsystem exampleSubsystem;
+    private final Set<Subsystem> subsystems;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase
-{
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    private final ExampleSubsystem subsystem;
-    
-    
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
-     */
-    public ExampleCommand(ExampleSubsystem subsystem)
-    {
-        this.subsystem = subsystem;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(subsystem);
+    public ExampleCommand(ExampleSubsystem exampleSubsystem) {
+        this.exampleSubsystem = exampleSubsystem;
+        this.subsystems = Set.of(this.exampleSubsystem);
     }
-    
-    
-    // Called when the command is initially scheduled.
+
     @Override
-    public void initialize() {}
-    
-    
-    // Called every time the scheduler runs while the command is scheduled.
+    public void initialize() {
+
+    }
+
     @Override
-    public void execute() {}
-    
-    
-    // Called once the command ends or is interrupted.
+    public void execute() {
+
+    }
+
     @Override
-    public void end(boolean interrupted) {}
-    
-    
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished()
-    {
+    public boolean isFinished() {
+        // TODO: Make this return true when this Command no longer needs to run execute()
         return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return this.subsystems;
     }
 }
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,24 +5,21 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 
 
-public class ExampleSubsystem extends SubsystemBase
+public class ExampleSubsystem implements Subsystem
 {
     /** Creates a new ExampleSubsystem. */
     public ExampleSubsystem() {}
-    
-    
-    @Override
+
     public void periodic()
     {
         // This method will be called once per scheduler run
     }
     
     
-    @Override
     public void simulationPeriodic()
     {
         // This method will be called once per scheduler run during simulation
